e5453edfc87e0438137281c262f0bd4a0f0b01ab
Mateo Ochoa <mateo.ochoac@gmail.com>
2022-12-28 15:13:29 -0400

    Finish Chapter 3

---
+++ Chapter-3-WorldTrotter/Chapter-3-WorldTrotter/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Chapter-3-WorldTrotter
+//
+//  Created by Mateo Ochoa on 2022-12-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor, UIColor.blue.cgColor, UIColor.purple.cgColor].reversed()
+        gradientLayer.locations = [0.1, 0.2, 0.3, 0.4, 1]
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
+

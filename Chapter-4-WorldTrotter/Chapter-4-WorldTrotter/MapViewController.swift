fb81cba04c6ed90bce85a2e0f69b644890989c1a
Mateo Ochoa <mateo.ochoac@gmail.com>
2023-01-20 18:23:06 -0400

    Finish chapter 5 and also finish bronze challenge

--- Chapter-4-WorldTrotter/Chapter-4-WorldTrotter/MapViewController.swift
+++ Chapter-4-WorldTrotter/Chapter-4-WorldTrotter/MapViewController.swift
@@ -6,10 +6,123 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
+        
+        addSegmentedControlSubview(view: view)
+        addPointsOfInterestSwitch(view: view)
+    }
+    
+    func addSegmentedControlSubview(view: UIView) {
+        // Adding segmentedControl to the view
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        // Adding constraints to segmentedControl
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        // Adding a target-action pair
+        segmentedControl.addTarget(self, action: #selector(mapTypeChanged(_:)), for: .valueChanged)
+    }
+    
+    func addPointsOfInterestSwitch(view: UIView) {
+        let label = UILabel()
+        label.text = "Points of interest"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        let segControl = view.subviews.first { view in
+            let testedView = view.self as? UISegmentedControl
+            return testedView != nil
+        }!
+        view.addSubview(label)
+                
+        let topConstraint = label.topAnchor.constraint(equalTo: segControl.bottomAnchor, constant: 10)
+        let leadingConstraint = label.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        
+        let uiSwitch = UISwitch()
+        uiSwitch.isOn = true
+        uiSwitch.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(uiSwitch)
+        
+        uiSwitch.topAnchor.constraint(equalTo: segControl.bottomAnchor, constant: 10).isActive = true
+        uiSwitch.leadingAnchor.constraint(equalTo: label.trailingAnchor).isActive = true
+        
+        
+        uiSwitch.addTarget(self, action: #selector(pointsOfInterestChanged(_:)), for: .valueChanged)
+    }
+    
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
+    
+    @objc func pointsOfInterestChanged(_ uiSwitch: UISwitch) {
+        switch mapView.mapType {
+        case .standard:
+            var mapConf = MKStandardMapConfiguration()
+            toggle(mapConf, uiSwitch)
+        case .hybrid:
+            var mapConf = MKHybridMapConfiguration()
+            toggle(mapConf, uiSwitch)
+        default:
+            return
+        }
+    }
+    
+    func toggleStandard(_ mapConf: MKStandardMapConfiguration, _ uiSwitch: UISwitch) {
+        if uiSwitch.isOn {
+            mapConf.pointOfInterestFilter = MKPointOfInterestFilter.includingAll
+        } else {
+            mapConf.pointOfInterestFilter = MKPointOfInterestFilter.excludingAll
+        }
+        mapView.preferredConfiguration = mapConf
+    }
+    
+    func toggleHybrid(_ mapConf: MKHybridMapConfiguration, _ uiSwitch: UISwitch) {
+        if uiSwitch.isOn {
+            mapConf.pointOfInterestFilter = MKPointOfInterestFilter.includingAll
+        } else {
+            mapConf.pointOfInterestFilter = MKPointOfInterestFilter.excludingAll
+        }
+        mapView.preferredConfiguration = mapConf
+    }
+    
+    func toggle<T: PointsOfInterest>(_ mapConf: T, _ uiSwitch: UISwitch) {
+        var newMapConf = mapConf
+        if uiSwitch.isOn {
+            newMapConf.pointOfInterestFilter = MKPointOfInterestFilter.includingAll
+        } else {
+            newMapConf.pointOfInterestFilter = MKPointOfInterestFilter.excludingAll
+        }
+        mapView.preferredConfiguration = newMapConf as! MKMapConfiguration
     }
 }

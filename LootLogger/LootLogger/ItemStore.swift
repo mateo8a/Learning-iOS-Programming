97c4189883227073d4a0ee1ae0537bd625b667c0
Mateo Ochoa <mateo.ochoac@gmail.com>
2023-01-31 08:43:47 -0400

    Finish chapter

--- LootLogger/LootLogger/ItemStore.swift
+++ LootLogger/LootLogger/ItemStore.swift
@@ -19,6 +19,23 @@ class ItemStore {
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
         allItems.append(newItem)
+        // Add option to insert item at the top of the list
+//        allItems.insert(newItem, at: 0)
         return newItem
+    }
+    
+    func deleteItem(_ item: Item) {
+        if let index = allItems.firstIndex(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex { return }
+        
+        let movedItem = allItems[fromIndex]
+        allItems.remove(at: fromIndex)
+        allItems.insert(movedItem, at: toIndex)
+        allItems[toIndex] = movedItem
     }
 }

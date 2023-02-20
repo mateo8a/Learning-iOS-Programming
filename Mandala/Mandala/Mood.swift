28c5ae3523266645e9d0aae01dc0fdbe74573029
Mateo Ochoa <mateo.ochoac@gmail.com>
2023-02-20 17:32:05 -0400

    Add images and colors for moods to assets catalog

---
+++ Mandala/Mandala/Mood.swift
@@ -0,0 +1,14 @@
+//
+//  Mood.swift
+//  Mandala
+//
+//  Created by Mateo Ochoa on 2023-02-20.
+//
+
+import UIKit
+
+struct Mood {
+    var name: String
+    var image: UIImage
+    var color: UIColor
+}
---
+++ Mandala/Mandala/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mandala
+//
+//  Created by Mateo Ochoa on 2023-02-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

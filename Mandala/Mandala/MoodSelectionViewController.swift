020c4d4c773dff4918bbf6aa5f173a5d84cb56af
Mateo Ochoa <mateo.ochoac@gmail.com>
2023-02-20 18:44:55 -0400

    Add moods stack

---
+++ Mandala/Mandala/MoodSelectionViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Mandala
+//
+//  Created by Mateo Ochoa on 2023-02-20.
+//
+
+import UIKit
+
+class MoodSelectionViewController: UIViewController {
+
+    @IBOutlet var stackView: UIStackView!
+    @IBOutlet var addMoodButton: UIButton!
+    
+    var moods = [Mood]() {
+        didSet {
+            currentMood = moods.first
+            moodButtons = moods.map { mood in
+                let moodButton = UIButton()
+                moodButton.setImage(mood.image, for: .normal)
+                moodButton.imageView?.contentMode = .scaleAspectFit
+                moodButton.addTarget(self, action: #selector(moodSelectionChanged(_:)), for: .touchUpInside)
+                return moodButton
+            }
+        }
+    }
+    var moodButtons = [UIButton]() {
+        didSet {
+            oldValue.forEach { mood in
+                mood.removeFromSuperview()
+            }
+            moodButtons.forEach{ stackView.addArrangedSubview($0) }
+        }
+    }
+    
+    var currentMood: Mood? {
+        didSet {
+            guard let currentMood = currentMood else {
+                addMoodButton.setTitle(nil, for: .normal)
+                addMoodButton.backgroundColor = nil
+                return
+            }
+            addMoodButton.setTitle("I'm \(currentMood.name)", for: .normal)
+            addMoodButton.backgroundColor = currentMood.color
+        }
+    }
+    
+    @objc func moodSelectionChanged(_ sender: UIButton) {
+        guard let selectedIndex = moodButtons.firstIndex(of: sender) else {
+            preconditionFailure("Unable to find the tapped button in the buttons array.")
+        }
+        currentMood = moods[selectedIndex]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        moods = [.happy, .sad, .angry, .goofy, .crying, .confused, .sleepy, .meh]
+        addMoodButton.layer.cornerRadius = addMoodButton.bounds.height / 2
+    }
+
+
+}
+
---
+++ Mandala/Mandala/UIColor+Mandala.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+Mandala.swift
+//  Mandala
+//
+//  Created by Mateo Ochoa on 2023-02-20.
+//
+
+import UIKit
+
+extension UIColor {
+    static let angry = UIColor(named: "angryRed")!
+    static let confused = UIColor(named: "confusedPurple")!
+    static let crying = UIColor(named: "cryingLightBlue")!
+    static let goofy = UIColor(named: "goofyOrange")!
+    static let happy = UIColor(named: "happyTurquoise")!
+    static let meh = UIColor(named: "mehGray")!
+    static let sad = UIColor(named: "sadBlue")!
+    static let sleepy = UIColor(named: "sleepyLightRed")!
+}

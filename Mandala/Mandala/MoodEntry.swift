7e15461775e703db90d0ef991ea8d9466c140307
Mateo Ochoa <mateo.ochoac@gmail.com>
2023-02-21 13:48:21 -0400

    Finish chapter 17

---
+++ Mandala/Mandala/MoodEntry.swift
@@ -0,0 +1,13 @@
+//
+//  MoodEntry.swift
+//  Mandala
+//
+//  Created by Mateo Ochoa on 2023-02-20.
+//
+
+import UIKit
+
+struct MoodEntry {
+    var mood: Mood
+    var timestamp: Date
+}
---
+++ Mandala/Mandala/MoodsConfigurable.swift
@@ -0,0 +1,12 @@
+//
+//  MoodsConfigurable.swift
+//  Mandala
+//
+//  Created by Mateo Ochoa on 2023-02-21.
+//
+
+import UIKit
+
+protocol MoodsConfigurable {
+    func add(_ moodEntry: MoodEntry)
+}

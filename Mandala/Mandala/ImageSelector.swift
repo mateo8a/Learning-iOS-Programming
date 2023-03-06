4bdc4d2cc5fe0155cf8f11f29031b4484b713c60
Mateo Ochoa <mateo.ochoac@gmail.com>
2023-03-06 15:19:36 -0600

    Use custom UIControl for mood selection

---
+++ Mandala/Mandala/ImageSelector.swift
@@ -0,0 +1,76 @@
+//
+//  ImageSelector.swift
+//  Mandala
+//
+//  Created by Mateo Ochoa on 2023-03-06.
+//
+
+import UIKit
+
+class ImageSelector: UIControl {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureViewHierarchy()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureViewHierarchy()
+    }
+    
+    var selectedIndex = 0
+    
+    private var imageButtons = [UIButton]() {
+        didSet {
+            oldValue.forEach { $0.removeFromSuperview() }
+            imageButtons.forEach { selectorStackView.addArrangedSubview($0) }
+        }
+    }
+    
+    var images = [UIImage]() {
+        didSet {
+            imageButtons = images.map { image in
+                let imageButton = UIButton()
+                
+                imageButton.setImage(image, for: .normal)
+                imageButton.imageView?.contentMode = .scaleAspectFit
+                imageButton.addTarget(self, action: #selector(imageButtonTapped(_:)), for: .touchUpInside)
+                
+                return imageButton
+            }
+            selectedIndex = 0
+        }
+    }
+    
+    @objc private func imageButtonTapped(_ sender: UIButton) {
+        guard let buttonIndex = imageButtons.firstIndex(of: sender) else {
+            preconditionFailure("The buttons and images are not parallel.")
+        }
+        selectedIndex = buttonIndex
+        sendActions(for: .valueChanged)
+    }
+    
+    private let selectorStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.spacing = 12.0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
+    private func configureViewHierarchy() {
+        addSubview(selectorStackView)
+        
+        NSLayoutConstraint.activate([
+            selectorStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            selectorStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            selectorStackView.topAnchor.constraint(equalTo: topAnchor),
+            selectorStackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}

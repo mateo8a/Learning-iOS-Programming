b1b7c3f7894692a1141bf02a9ccce195d4868218
Mateo Ochoa <mateo.ochoac@gmail.com>
2023-03-07 17:58:36 -0600

    Finish frontend scaffold for app

---
+++ Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Mateo Ochoa on 2023-03-07.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    @IBOutlet private var imageView: UIImageView!
+}
+

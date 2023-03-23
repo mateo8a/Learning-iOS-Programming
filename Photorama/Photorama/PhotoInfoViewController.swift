96dbf3898fa3dd259d55d1cb95ff0d07b3bd5b8d
Mateo Ochoa <mateo.ochoac@gmail.com>
2023-03-23 12:12:42 -0600

    Show picture when tapped in the collection

---
+++ Photorama/Photorama/PhotoInfoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoInfoViewController.swift
+//  Photorama
+//
+//  Created by Mateo Ochoa on 2023-03-23.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var photo: Photo! {
+        didSet {
+            navigationItem.title = photo.title
+        }
+    }
+    
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchImage(for: photo) { result in
+            switch result {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+        }
+    }
+}
